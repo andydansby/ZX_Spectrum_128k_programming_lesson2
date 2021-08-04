ec61670f98ca940a11be077c7bc3c62e23a1181d
Andrew Dansby <andydansby@gmail.com>
2021-08-04 07:04:22 -0400

    Add files via upload

--- RAM0/ram0.c
+++ RAM0/ram0.c
@@ -5,8 +5,6 @@
 
 // this has been pushed to zpragma.inc
 //#pragma output CRT_ORG_CODE = 49152
-//RAM 0 bankable
-//#pragma include:zpragma.inc
 
 //be sure to bank to this slot if you
 //plan on using other banks
@@ -15,27 +13,8 @@
 #include <input.h>
 #include <stdlib.h>//standard library
 #include <stdio.h>//standard input output
-//#include <intrinsic.h>//temp to place labels
-//powerful troubleshooting tool
 
 //we are just putting functions here
-
-
-//if this is before any custom headers
-//it will set everything below it to be
-//in the correct memory slot
-
-/*
-void placement_in_bank0(void)
-{
-    __asm
-    SECTION BANK_00
-    PUBLIC _someVariable
-    _someVariable:
-    defb 25
-    __endasm
-}
-*/
 
 #include "ram0.h"
 
